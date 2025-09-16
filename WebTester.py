916e1ac6753cc075c14ddbcf0f4b0ff8027b826e
Gaia Iturralde <gaiaiturralde@linux204.csc.uvic.ca>
2025-09-15 20:19:39 -0700

    Program can accept URI using sys library

--- WebTester.py
+++ WebTester.py
@@ -1,1 +1,13 @@
-import socket
+from socket import *
+import sys
+
+uri = sys.argv[1]
+
+# Parse uri string here
+# Use uri.split("/") or/and uri.split(".")
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(("www.google.ca", 80))
+s.send("GET/index.html HTTP/1.0\n\n")
+data = s.recv(10000)
+s.close()
